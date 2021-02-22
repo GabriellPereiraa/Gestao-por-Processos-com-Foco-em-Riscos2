5b6be2e765e6fbd11f5595b9799bc22604185502
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-22 14:35:42 -0300

    Create Topico01.php

---
+++ Topico01.php
@@ -0,0 +1,173 @@
+<?php
+// HEADER
+include('layout/header.php');
+?>
+
+<body>
+    <!-- PLUGIN DO FADE IN -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+
+    <!-- MENU -->
+    <?php include('layout/menu.php') ?>
+
+    <div id="main">
+        <!-- NAV TOP -->
+        <?php include('layout/nav.php') ?>
+        <!-- CONTEUDO INICIO -->
+        <div class="main-content container-fluid">
+            <h2 class="font-bold Azul">
+                <i class="fas fa-folder"></i> Tópico 01 – Riscos: O que são e os que nos interessam
+            </h2>
+
+            <hr class="hr4">
+
+            <p class="Texto"><strong>Objetivo:</strong>  Apresentar informações que permitam a compreensão das características dos riscos em geral e daqueles que interessam às organizações.
+            </p>
+            
+            <hr class="hrLaranja">
+
+            <h3 class="font-bold Azul">
+                <i class="fas fa-folder"></i> 01.1 - O que são e o que não são riscos?
+            </h3>
+            <hr class="hr4">
+
+            <p class="Texto">
+            A definição para risco pode ser considerada circunstancial e dependente de quem ou qual organização a apresenta ou a considera em sua vida, pessoal ou organizacional. Nada obstante, indubitavelmente, o risco traz em sua essência uma imagem de algo sobre o qual não se pode prever ou evitar, algo incerto, um evento que pode ou não ocorrer, normalmente acarretando certa perda, em caso de se tornar realidade. 
+            </p>
+
+            <div class="col-md-12">
+                <blockquote class="blockquoteEad">
+                    <p class="mb-0">“O risco é parte de qualquer empreitada humana. Desde o instante em que despertamos pela manhã, que entramos em nosso carro ou usamos o transporte público para a escola ou trabalho, até voltarmos para a cama (e talvez mesmo depois), estamos expostos a riscos de diferentes intensidades.”</p>
+                    <footer class="blockquote-footer">(DAMODARAN, 2009, p. 21.) –</footer>
+                </blockquote>
+            </div>
+            <br>
+
+            <p class="Texto">
+                Complementando essa visão relativa a uma abordagem humana sobre risco, Hill afirma que
+            </p>
+
+            <div class="col-md-12">
+                <blockquote class="blockquoteEad">
+                    <p class="mb-0">“o risco reflete coisas futuras com as quais estamos preocupados, como nossa situação financeira, nossa saúde, nossos empregos, nossos filhos, o meio ambiente, os outros países, as leis e as regulações e os futuros políticos.”</p>
+                    <footer class="blockquote-footer">( HILL, 2006, p. 37) –</footer>
+                </blockquote>
+            </div>
+            <br>
+
+            <p class="Texto">
+                Nesse contexto, faz-se necessário demarcar um ponto de partida, sendo razoável supor que inicialmente seja interessante definir o que é <strong>incerto, incerteza e evento</strong>, para na seqüência tratar-se especificamente o risco e seu gerenciamento. 
+            </p>
+            <p class="Texto">
+                Com efeito, dentre inúmeras definições, pode-se considerar <strong>incerto</strong> como algo que não é certo ou que é duvidoso, que transmite dúvidas, que não é seguro, algo contingente ou aleatório, e a <strong>incerteza</strong> como a falta de certeza ou a dúvida, ou o estado de coisa incerta (AURÉLIO, 2017). 
+            </p>
+            <p class="Texto">
+                Já<strong>evento</strong> está associado a um acontecimento, a algo que acontece (PRIBERAM, 2017), um incidente ou ocorrência, a partir de fontes internas ou externas a uma entidade, podendo afetar a realização dos objetivos (COSO, 2007). 
+            </p>
+            <p class="Texto">
+                Assim, pode-se presumir que o risco está relacionado a situações, a estados ou a eventos em que não se pode assegurar que algo ocorra com certeza, sobre o que restam dúvidas ou não se está seguro, de forma imprevisível, contingencial, aleatória. 
+            </p>
+            <p class="Texto">
+                Nesse sentido, de acordo com o Ministério da Economia e Finanças do Reino Unido,
+            </p>
+
+            <div class="col-md-12">
+                <blockquote class="blockquoteEad">
+                    <p class="mb-0">“risco é definido como incerteza do resultado, seja em forma de uma oportunidade (positiva) ou de uma ameaça (negativa), de ações e eventos.” </p>
+                    <footer class="blockquote-footer">( HM TREASURY, 2004) –</footer>
+                </blockquote>
+            </div>
+            <br>
+
+            <p class="Texto">
+                De outra parte, o <span style="color:#FC6B30">Committee of Sponsoring Organizations of the Treadway Commission(</span>Comitê das Organizações Patrocinadoras da Comissão Treadway<span style="color:#FC6B30">)</span>definiu que
+            </p>
+            
+            <div class="col-md-12">
+                <blockquote class="blockquoteEad">
+                    <p class="mb-0">“incertezas representam riscos e oportunidades, com potencial para destruir ou agregar valor às organizações, as quais só existem para gerar valor para os <span style="color:#FC6B30">stakeholders(</span>partes interessadas<span style="color:#FC6B30">)</span></p>
+                    <footer class="blockquote-footer">( COSO, 2007, p. 3) –</footer>
+                </blockquote>
+            </div>
+            <br>
+
+            <p class="Texto">
+            De acordo com IFAC (2001, p. 57), os stakeholders (partes interessadas) abrangem: o corpo ou indivíduo (s) eleito(s) ou designado(s) para a diretoria de um órgão e que têm responsabilidade pela avaliação do desempenho; os representantes eleitos pela sociedade (parlamentares); os provedores de recursos (contribuintes, credores financeiros, proprietários de títulos do governo e credores em geral); os prestadores de serviços e parceiros (funcionários, contratados, parceiros de joint ventures e de outras entidades governamentais); os usuários dos serviços (pessoas físicas e empresas que se beneficiam dos serviços que a entidade proporciona); grupos de interesse como analistas e coletores de estatísticas (analistas políticos, economistas, analistas financeiros, agências de rating); a mídia; e a comunidade em geral.
+            </p>
+
+            <div>
+                    <p class="SaibaMaisTitulo"> Saiba Mais</p>
+                    <p class="SaibaMaisTexto">
+                        O <span style="color: #FC6B30;">COSO®(</span>(Committee of Sponsoring Organizations of the Treadway Commission <span style="color: #FC6B30;">)</span> é uma organização privada criada nos EUA, em 1985, para prevenir e evitar fraudes nos procedimentos e processos internos da empresa. 
+                    </p>
+                    <p class="SaibaMaisTexto">
+                        Inicialmente criada como <span style="color: #FC6B30;">National Commission on Fraudulent Financial Reporting(</span>National Commission on Fraudulent Financial Reporting<span style="color: #FC6B30;">)</span>, essa comissão era formada por representantes das principais associações de classes de profissionais ligados à área financeira.
+                    </p>
+                    <p class="SaibaMaisTexto">
+                        O primeiro objeto de estudo da comissão foram os controles internos das empresas. Essa comissão, posteriormente, tornou-se um comitê e passou a se chamar COSO - Committee of Sponsoring Organizations of the Treadway Commission.
+                    </p>
+                    <p class="SaibaMaisTexto">
+                        O COSO é uma organização sem fins lucrativos, dedicada à melhoria dos relatórios financeiros, sobretudo pela aplicação da ética e da efetividade na aplicação e no cumprimento dos controles internos e é patrocinado pelas cinco das principais associações de classe de profissionais ligados à área financeira nos EUA. Em decorrência da globalização e padronização internacional das técnicas de auditoria, as recomendações da COSO, relativas aos controles internos, bem como seu cumprimento e sua observância são amplamente praticados e tidos como modelo e referência no Brasil e na maioria dos países do mundo.
+                    </p>
+                    <p class="SaibaMaisTexto">
+                    Fonte: <a href="https://pt.wikipedia.org/wiki/COSO">https://pt.wikipedia.org/wiki/COSO</a>, Acesso em 15 ago. 2017.
+                    </p>
+                </div>
+                <br>
+
+                <p class="Texto">
+                    Ademais, e com a finalidade de demonstrar a relação entre eventos, riscos e oportunidades, COSO (2007, p. 4) indica que 
+                </p>
+
+                <div class="col-md-12">
+                    <blockquote class="blockquoteEad">
+                        <p class="mb-0">“os eventos podem gerar impacto tanto negativo quanto positivo ou ambos. Os que geram impacto negativo representam riscos que podem impedir a criação de valor ou mesmo destruir o valor existente. Os de impacto positivo podem contrabalançar os de impacto negativo ou podem representar oportunidades, que por sua vez representam a possibilidade de um evento ocorrer e influenciar favoravelmente a realização dos objetivos, apoiando a criação ou a preservação de valor. ”</p>
+                        <footer class="blockquote-footer">( COSO, 2007, p. 4) –</footer>
+                    </blockquote>
+                </div>
+                <br>
+
+                <p class="Texto">
+                    Diante de toda a variedade, e não dotada de consenso, de definições expostas, pode-se inferir que a incerteza (algo que qualifica o risco), o risco e a oportunidade mantêm entre si relação de complementaridade em seus significados, e que o risco pode ser categorizado em termos das consequências positivas (oportunidades) e negativas (aqui entendidas como ameaças) decorrentes de sua ocorrência, por meio de um evento.
+                </p>
+                <p class="Texto">
+                    Finalmente, <strong>algo é certo e indiscutível: problemas, fatos ou eventos passados, que já ocorreram, não são riscos</strong>, justamente porque, em virtude de já terem ocorrido, não são mais detentores de característica ou atributo desconhecido, em si mesmo e em termos de seus efeitos, sejam eles positivos ou negativos. 
+                </p>
+
+                <div>
+                    <p class="CuriosidadeTitulo"> Dica</p>
+                    <p class="CuriosidadeTexto"><strong>Sugestão de leitura complementar: Capítulo 1 - “O que é risco?”, do livro de Damadoran (2009), p. 21-24.</strong>
+                    </p>
+                </div>
+
+            <br><br>
+            <div class="center">
+                <div class="btn-group" id="btn-group">
+                    <a href="Apresentacao.php" class="btn btn-outline-success btn-sm">Página Anterior</a>
+                    <a href="Topico02.1.php" class="btn btn-outline-success btn-sm">Proxima Página</a>
+                </div>
+            </div>
+
+            <script>
+                $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                    event.preventDefault();
+                    $(this).ekkoLightbox();
+                });                    
+            </script>
+
+        </div> <!-- CONTEUDO FIM -->
+
+        <!-- FOOTER -->
+        <?php include('layout/footer.php') ?>
+
+    </div> <!-- DIV MAIN FIM -->
+
+    <!-- FOOTER JS -->
+    <?php include('layout/js.php') ?>
+
+</body>
+
+</html>
