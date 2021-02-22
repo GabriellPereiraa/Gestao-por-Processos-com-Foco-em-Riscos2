d9e849ca373aacc4ed163e3a39f16496ab36fa01
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-22 16:59:06 -0300

    Create Topico02.3

---
+++ Topico02.3.php
@@ -0,0 +1,172 @@
+<?php
+// HEADER
+include('layout/header.php');
+?>
+
+<body>
+    <!-- PLUGIN DO FADE IN -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+
+    <!-- MENU -->
+    <?php include('layout/menu.php') ?>
+
+    <div id="main">
+        <!-- NAV TOP -->
+        <?php include('layout/nav.php') ?>
+        <!-- CONTEUDO INICIO -->
+        <div class="main-content container-fluid">
+            <h2 class="font-bold Azul">
+                <i class="fas fa-folder"></i> 02.3 - Como gerenciar riscos?
+            </h2>
+
+            <hr class="hr4">
+
+            <p class="Texto">
+                Após o que até aqui foi apresentado em termos de riscos, inclusive o que são e o que não são riscos, aqueles que interessam, os motivos para que sejam gerenciados e o que esse gerenciamento significa, serão agora apresentadas algumas formas de se efetuarem os procedimentos de gestão de riscos.  
+            </p>
+            <p class="Texto">
+            Nesse sentido, registre-se a existência de alguns modelos ou abordagens de referência que apresentam metodologias e padrões a serem utilizados para a gestão de riscos, destacando-se, dentre os mais utilizados, o que se refere à <stron>Norma ISO 31000-2018</stron> e à norma concebida pelo Committee of Sponsoring Organizations of the Treadway Commission ou simplesmente <stron>COSO</stron>, em sua versão <stron>II</stron>, dedicados à melhoria dos relatórios financeiros por meio da ética, efetividade dos controles internos e governança corporativa. Essa publicação da norma substituia anterior que era de 2009 e traz algumas importantes diferenças.
+            </p> 
+            <p class="Texto">
+                Acerca do modelo previsto na Norma ISO 31000-2018, registre-se que se refere a uma série de padrões relacionados ao gerenciamento de riscos, codificados pela International Organizations for Standadization, tendo surgido da necessidade de harmonizar padrões e regulamentações publicados anteriormente, relacionados com a gestão de riscos. 
+            </p>
+            <p class="Texto">
+                Tal norma pode ser aplicada aos vários tipos de riscos, ligados aos diferentes setores de uma entidade, incluindo a visão de que risco também é oportunidade. O escopo dessa abordagem é permitir que todas as tarefas estratégicas, gerenciais e operacionais de uma organização estejam alinhadas com objetivos próprios da gestão de riscos, por meio de tarefas e processos.
+            </p>
+            <p class="Texto">
+                A aplicação do modelo da Norma ISO trata de formalizar e de desenvolver processos dentro de Sistemas de Gerenciamento de Riscos. A maioria das implicações, por adotar o novo padrão, está relacionada à reengenharia de práticas gerenciais, para entrar em conformidade com a documentação, comunicação e socialização do novo padrão de operação de Gestão de Riscos.
+            </p>
+
+            <div class="text-center">
+                <p class="TituloFigura"><strong>Imagem 3:</strong> Norma ISO 3100-2009 (AS/NZS 4360)</p>
+                <a href="imagens/Imagem03.png" data-toggle="lightbox" data-footer="Norma ISO 3100-2009 (AS/NZS 4360)">
+                    <img class="img-fluid" src="imagens/Imagem03.png" alt="Alt_da_Imagem">
+                </a>
+            </div>
+            <br>
+
+            <p class="Texto">
+                Comparando as duas versões podemos elencar alguns pontos: 
+            </p>
+            <p class="Texto">
+            <strong>1.</strong> a introdução da norma ficou mais sucinta e objetiva;
+            </p>
+            <p class="Texto">
+            <strong>2.</strong> termos e definições houve uma redução, pois na versão 2009 haviam 29 itens enquanto que na versão 2018 restaram apenas 8. 
+            </p>
+            <p class="Texto">
+            <strong>3.</strong> os princípios também houve redução de 11, na versão 2009, para 8 na versão 2018;
+            </p>
+            <p class="Texto">
+            <strong>4.</strong> a estrutura sofreu diversas mudanças, mas sem sem muitas alterações de sentido.
+            </p>
+
+            <p class="Texto">
+                Algumas ideias ganharam mais destaque do que na versão anterior, sendo:
+            </p>
+            <p class="Texto">
+            <strong>a)</strong> Posicionamento da liderança: Assim como em outras normas ISO mais recentes, a liderança, ou Alta Direção, vem sendo posicionada como essencial para a condução das atividades de definição de responsabilidade e, especialmente, mudança de cultura por meio da demonstração de comprometimento.
+            </p>
+            <p class="Texto">
+            <strong>b)</strong> Monitoramento sistêmico: Na nova versão é pontuado sobre a necessidade de assegurar que sistemas para gerenciar riscos estejam implementados e operem eficazmente, lembrando que sistemas podem ser informatizados ou não.
+            </p>
+            <p class="Texto">
+            <strong>c)</strong> Contexto da organização: Ao se analisar o contexto da organização, foi incluído um ponto sobre considerar a complexidade das redes de relacionamento e dependências, de partes interessadas.
+            </p>
+            <p class="Texto">
+            <strong>d)</strong> Comunicação e consulta: É ressaltada a importância da comunicação e consulta com as partes interessadas de forma mais genérica, porém destacando a relevância de desenvolver melhorias a partir das informações coletadas com a consulta.
+            </p>
+            <p class="Texto">
+            <strong>e)</strong> Implementação: A nova redação tem mais foco em planejamento da estrutura, retirando citações objetivas referentes a manutenção de registros e atendimento a requisitos legais, que estavam presentes na versão anterior.
+            </p>
+
+            <p class="Texto">
+            Na versão 2018 foi dada ênfase do caráter dinâmico e iterativo da gestão de riscos; assim como as orientações para identificação, análise e avaliação de riscos ficaram mais claras e didáticas; também a abordagem sobre documentação do processo de gestão de riscos e seus resultados ficou mais flexível; não há mais a presença do anexo que explicava os atributos de uma gestão de riscos avançada; empregou um discurso mais gerencial, ou estratégico; e foi reforçada a ideia de que a gestão de riscos é algo que deva ser intrínseca à organização.
+            </p>
+            <p class="Texto">
+            Já o modelo COSO II aborda o paradigma de gestão de riscos por meio de um modelo integrado composto pelas dimensões: ambiente interno; definição de objetivos; identificação de eventos; avaliação de riscos; resposta ao risco; atividades de controle; informação e comunicação; e monitoramento (COSO, 2007). As especificações, alcances e significados desse modelo integrado serão apresentados na próxima seção desta aula. 
+            </p>
+            <p class="Texto">
+            Por outro lado, Hill (2006) lembra que foram desenvolvidas várias outras estruturas de gestão de riscos com a finalidade de auxiliar os gestores a tratar os riscos de forma sistemática, citando, a título de exemplo: a gestão de riscos da Associação Canadense de Padrões (CAN/CSAQ850-97); o modelo de Critérios de Controle desenvolvido pelo <span style="color:#FC6B30;"> Instituto Canadense de Auditores Juramentados (</span>Canadian Institute of Chartered Accountants<span style="color: #FC6B30;">)</span>; a Estrutura de Gestão de Riscos em Políticas Públicas, do Grupo de Trabalho de Secretários Executivos Adjuntos para a Gestão de Riscos; e a estrutura para a Gestão Integrada de Riscos, do Conselho do Tesouro canadense. 
+            </p>
+            <p class="Texto">
+            Hill (2006) destaca ainda que essas estruturas de gestão de risos desenvolvidas estão suportadas por uma base comum referente às principais etapas de um ciclo de processo decisório em gestão de riscos: identificação; avaliação; resposta; comunicação; e monitoramento e aprendizagem.
+            </p>
+
+            <div class="text-center">
+                <p class="TituloFigura"><strong>Imagem 4:</strong> Ciclo de Processo Decisório em Gestão de Riscos</p>
+                <a href="imagens/Imagem04.png" data-toggle="lightbox" data-footer="Ciclo de Processo Decisório em Gestão de Riscos">
+                    <img class="img-fluid" src="imagens/Imagem04.png" alt="Alt_da_Imagem">
+                </a>
+            </div>
+            <br>
+
+            <p class="Texto">
+                Ademais, registre-se que a CGU e o MPOG (CGU, 2016) estabeleceram, no âmbito da IN nº 01/2016, que na implementação e na atualização do modelo de gestão de riscos, a alta administração, bem como seus servidores ou funcionários, deverá observar os componentes da estrutura de gestão de riscos: 
+            </p>
+
+            <p class="Texto">
+            <i class="fas fa-arrow-right" style="color:#FC6B30;"></i> ambiente interno; 
+            </p>
+            <p class="Texto">
+            <i class="fas fa-arrow-right" style="color:#FC6B30;"></i> fixação de objetivos; 
+            </p>
+            <p class="Texto">
+            <i class="fas fa-arrow-right" style="color:#FC6B30;"></i> identificação de eventos; 
+            </p>
+            <p class="Texto">
+            <i class="fas fa-arrow-right" style="color:#FC6B30;"></i> avaliação de riscos;  
+            </p>
+            <p class="Texto">
+            <i class="fas fa-arrow-right" style="color:#FC6B30;"></i> resposta a riscos; 
+            </p>
+            <p class="Texto">
+            <i class="fas fa-arrow-right" style="color:#FC6B30;"></i> atividades de controles internos; 
+            </p>
+            <p class="Texto">
+            <i class="fas fa-arrow-right" style="color:#FC6B30;"></i> informação e comunicação; e  
+            </p>
+            <p class="Texto">
+            <i class="fas fa-arrow-right" style="color:#FC6B30;"></i> monitoramento.
+            </p>
+
+            <p class="Texto">
+                Diante do exposto, conclui-se que há diversas e variadas proposições e modelos destinados a viabilizar a gestão de riscos em uma organização e que a adoção de um modelo pode basear-se inclusive em uma estrutura concebida, modelada e definida pela própria organização, observando suas realidades institucionais e suas necessidades de decisões relacionadas a riscos. 
+            </p>
+
+            <div>
+                <p class="SaibaMaisTitulo"> Saiba Mais</p>
+                <p class="SaibaMaisTexto">Para uma leitura complementar, acesse a biblioteca do curso e leia o <a href="https://drive.google.com/file/d/0B5moywX9O3sQSDBkSDlKNUdRemc/view">" Guia s obre a gestão de riscos no serviço público"</a>, de Stephem Hill.</p>
+            </div>
+
+            <br><br>
+            <div class="center">
+                <div class="btn-group" id="btn-group">
+                    <a href="Topico01.2.php" class="btn btn-outline-success btn-sm">Página Anterior</a>
+                    <a href="Topico02.2.php" class="btn btn-outline-success btn-sm">Proxima Página</a>
+                </div>
+            </div>
+
+            <script>
+                $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                    event.preventDefault();
+                    $(this).ekkoLightbox();
+                });                    
+            </script>
+
+        </div> <!-- CONTEUDO FIM -->
+
+        <!-- FOOTER -->
+        <?php include('layout/footer.php') ?>
+
+    </div> <!-- DIV MAIN FIM -->
+
+    <!-- FOOTER JS -->
+    <?php include('layout/js.php') ?>
+
+</body>
+
+</html>
