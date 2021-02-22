15e1d053ce2b893f37e7d315cb8249b3a576cb8e
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-22 13:38:23 -0300

    start

---
+++ layout/menu.php
@@ -0,0 +1,60 @@
+<body>
+    <div id="app">
+        <div id="sidebar">
+            <div class="sidebar-wrapper active ps ps--active-y">
+                <div class="m-4">
+                    <a href="index.php">
+                        <img class="img-fluid" src="http://ava.egp.ce.gov.br/2018/desv/logo/egp.png" alt="Logo EGPCE" srcset="">
+                    </a>
+                </div>
+                <div class="sidebar-menu ">
+                    <ul class="menu">
+                        <!--  <li class="sidebar-item">
+                        <a href="../Apresentacao.php" class="sidebar-link">
+                            <i data-feather="home" width="20"></i>
+                            <span>Apresentação</span>
+                        </a>
+                    </li> -->
+                        <li class="sidebar-item">
+                            <a href="Apresentacao.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>Apresentação</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Topico01.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>Tópico 1 – Gestão por Processos: Contextualização</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item has-sub">
+                            <a href="" class='sidebar-link'>
+                                <i data-feather="home" width="20"></i>
+                                <span>Tópico 2 – Gestão de Processos</span>
+                            </a>
+                            <ul class="submenu">
+                                <li class="sidebar-item">
+                                    <a href="Topico02.1.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>2.1 - Gerenciamento para manter processos</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico02.2.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>2.2 - Gerenciamento para melhorar processos</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Referencias.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>Referências</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
