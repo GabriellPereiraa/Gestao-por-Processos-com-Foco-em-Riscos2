11511387c4483d5db4bbffe19d562b2fb2df9ae2
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-23 13:50:26 -0300

    Create Referencias.php

---
+++ Referencias.php
@@ -0,0 +1,136 @@
+<?php
+// HEADER
+include('layout/header.php');
+?>
+
+<body>
+    <!-- PLUGIN DO FADE IN -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    <!-- MENU -->
+    <?php include('layout/menu.php') ?>
+
+    <div id="main">
+        <!-- NAV TOP -->
+        <?php include('layout/nav.php') ?>
+        <!-- CONTEUDO INICIO -->
+
+        <div class="main-content container-fluid">
+            <div class="page-title">
+
+                <h3 class="font-bold">Referências</h3>
+                <hr>
+
+                <ul style="list-style: none;">
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> AVALOS, José Miguel Aguilera.  <strong>Auditoria e Gestão de Riscos </strong>. São Paulo: Saraiva, 2009.
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> BRAGA. Marcus Vinicius de Azevendo. <strong>Gestão de Riscos no Setor Público: </strong>ampliando horizontes. Disponível em <a href="https://administradores.com.br/artigos/gestao-dos-riscos-no-setor-publico-ampliando-os-horizontes">http://www.administradores.com.br/artigos/negocios/gestao-dosriscos-no-setor-publico-ampliando-os-horizontes/67507</a>. Acesso em 09/09/2017. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i>  BRITO, Claudenir; FONTENELLE, Rodrigo. <strong> Auditoria privada e governamental:</strong> teoria de forma objetiva e questões comentadas. Rio de Janeiro: Elsevier, 2013.
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i>  CGE/CE. <strong> Auditoria Governamental de Processos com Foco em Riscos</strong>, Ernest & Young Terco, 2012. 258p. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> CGE/CE. Disponível em <a href="https://www.cge.ce.gov.br/2017/08/30/seminario-controles-internos-gestao-de-riscos-e-programa-de-integridade/">https://www.cge.ce.gov.br/2017/08/30/seminario-controles-internos-gestao-de-riscos-e-programa-de-integridade/</a>. Acesso em 09 set. 2017.
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> CGU. Disponível em <a href="https://www.gov.br/cgu/sobre/legislacao/arquivos/instrucoes-normativas/in_cgu_mpog_01_2016.pdf">https://www.gov.br/cgu/sobre/legislacao/arquivos/instrucoes-normativas/in_cgu_mpog_01_2016.pdf</a>. Acesso em 15 ago. 2017. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> CHAVES, Renato Santos. <strong>Auditoria e Controladoria no Setor Público</strong> – Fortalecimento dos Controles Internos com Jurisprudência do TCU. 2 Ed. Curitiba: Juruá, 2011. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> COSO, <strong>Gerenciamento de Riscos na Empresa</strong> – Estrutura Integrada: Sumário Executivo e Estrutura (Versão em português), 2 vol. Jersey City: AICPA, 2007. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> DAMODARAN, Aswath. <strong>Gestão estratégica do risco</strong> – uma referência para a tomada de riscos empresariais. São Paulo: Bookman, 2009. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> DIAS, Sergio Vidal dos Santos. <strong>Auditoria de Processos Organizacionais:</strong> Teoria, Finalidade, Metodologia de Trabalho e Resultados Esperados - 3. ed – São Paulo: Atlas, 2011. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> ENAP – Fundação Escola Nacional de Administração Pública. <strong>Guia sobre a gestão de riscos no serviço público.</strong> Stephen Hill. In: ENAP Cadernos 30. Brasília: Enap, 2006.
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> HM Treasury. <strong>The Orange Book</strong> - Management of Risk - Principles and Concepts, 2004. 52p. Disponível em: <a href="https://goo.gl/wSqdZ6">https://goo.gl/wSqdZ6</a>
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> House of Commons Committee of Public Accounts, <strong>Managing risks to improve public services. </strong>2005. 40p. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> IFAC. International Federation of Accountants. Study 13. <strong>Corporate governance in the public sector:</strong> a governing body perspective. 2001. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> INTOSAI, INTOSAI GOV 9100 - The International Standards of Supreme Audit Institutions, ISSAI, are issued by the International Organization of Supreme Audit Institutions, 2007. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> PRIBERAM. Disponível em <a href="https://www.priberam.pt/dlpo/evento">https://www.priberam.pt/dlpo/evento</a>. Acesso em 08 ago. 2017.
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> QSP. Disponível em <a href=" http://www.qsp.org.br/pdf/implemente_abr.pdf">http://www.qsp.org.br/pdf/implemente_abr.pdf</a>. Acesso em 09 set. 2017. 
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> TCU, <strong>Auditoria Governamenta</strong>, 2011. 356p. Disponível em <a href="https://portal.tcu.gov.br/lumis/portal/file/fileDownload.jsp?fileId=8A8182A24F0A728E014F0B277355651D">https://portal.tcu.gov.br/lumis/portal/file/fileDownload.jsp?fileId=8A8182A24F0A728E014F0B277355651D</a>. Acesso em 20 jul. 2013
+                    </li>
+                    <br>
+                    <li>
+                        <i class="fa fa-book fa-lg CorLaranja" aria-hidden="true"></i> Victorian Government (Australia) – Department of Treasury and Finance, <strong>Victorian Government Risk Management Framework</strong>, 2011. 38p Disponível em <a href=" https://www.dtf.vic.gov.au/planning-budgeting-andfinancial-reporting-frameworks/victorian-risk-managementframework-and-insurance-management-policy"> https://www.dtf.vic.gov.au/planning-budgeting-andfinancial-reporting-frameworks/victorian-risk-managementframework-and-insurance-management-policy</a> 20 jul. 2013.
+                    </li>
+                    <br>
+                </ul>
+            </div>
+
+        </div>
+
+        <br>
+        <div class="center">
+            <div class="btn-group" id="btn-group">
+                <a href="Topico02.2.php" class="btn btn-outline-success btn-sm ">Página Anterior</a>
+                <a href="#" class="btn btn-outline-success btn-sm">Avaliação</a>
+            </div>
+        </div>
+
+        <!-- SCRIPT LIGHTBOX -->
+        <script>
+                    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                        event.preventDefault();
+                        $(this).ekkoLightbox();
+                    });                    
+                </script>
+
+    </div> <!-- CONTEUDO FIM -->
+
+<!-- FOOTER -->
+<?php include('layout/footer.php') ?>
+
+</div> <!-- DIV MAIN FIM -->
+
+<!-- FOOTER JS -->
+<?php include('layout/js.php') ?>
+
+</body>
+
+</html>
